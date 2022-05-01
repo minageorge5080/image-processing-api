b5c93eecfacfe132a82ba1e0628625e66dc7e7d4
Mina George <mgeorge@minlyapp.com>
2022-05-01 04:44:23 +0200

    Fix lint errors

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 const isNumeric = (value: unknown): boolean => {
   if (typeof value != "string") return false;
-  else return /^-?\d+$/.test(value);
+  return /^-?\d+$/.test(value);
 };
 
 export default isNumeric;
